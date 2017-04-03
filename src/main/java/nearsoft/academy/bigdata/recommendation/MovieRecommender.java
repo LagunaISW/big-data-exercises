27df15f6c6d5438872dbf8fef51c1b9cb4d2e5fc
Jhonatan Laguna <hola@jhonatanlaguna.com>
2017-04-02 21:47:59 -0700

    All test passed

--- src/main/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/main/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -1,7 +1,23 @@
 package nearsoft.academy.bigdata.recommendation;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,45 +26,84 @@ import java.util.List;
  */
 public class MovieRecommender {
 
+    /* CONFIGURATION */
+        public static final int PRODUCT_BEGIN_INDEX = 19;
+        public static final int USER_BEGIN_INDEX = 15;
+        public static final int SCORE_BEGIN_INDEX = 14;
+        public static final String PRODUCT_PATTERN = "product/productId: ";
+        public static final String USER_PATTERN = "review/userId: ";
+        public static final String SCORE_PATTERN = "review/score: ";
+        public static final String DATABASE = "database.txt";
+    /* CONFIGURATION */
+
     private int totalReviews;
     private HashMap<String, Integer> totalUsers;
-    private HashMap<String, Integer> totalProducts;
+    private HashBiMap<String, Integer> totalProducts;
+    private Recommender recommender;
 
     public MovieRecommender(String pathToFile) {
-        this.totalProducts = new HashMap<String, Integer>();
+        this.totalProducts = HashBiMap.create();
         this.totalUsers = new HashMap<String, Integer>();
-        setValues(pathToFile);
+        File database = setValues(pathToFile);
+        buildRecommender(database);
     }
 
-    private void setValues(String path){
+    private File setValues(String filePath) {
         try {
-            BufferedReader reader = GzipReader.readGzip(path);
+            BufferedReader reader = GzipReader.readGzip(filePath);
+            FileWriter writer = new FileWriter(DATABASE);
 
-            String line;
+            String lineToRead;
+            String lineToWrite = "";
+            int selectedProduct = 0;
 
-            while((line = reader.readLine()) != null) {
-                if (line.startsWith("product/productId: ")) {
-                    String productId = line.substring(19);
-                    if (!this.totalProducts.containsKey(productId))
+            while((lineToRead = reader.readLine()) != null) {
+
+                if(lineToRead.startsWith(PRODUCT_PATTERN)) {
+                    String productId = lineToRead.substring(PRODUCT_BEGIN_INDEX);
+                    if (!this.totalProducts.containsKey(productId)){
                         this.totalProducts.put(productId, this.totalProducts.size());
+                        selectedProduct = this.totalProducts.get(productId);
+                    }
+                } else if (lineToRead.startsWith(USER_PATTERN)) {
+                    String userId = lineToRead.substring(USER_BEGIN_INDEX);
 
-                }
-
-                else if (line.startsWith("review/userId: ")) {
-                    String userId = line.substring(15);
-                    if (!this.totalUsers.containsKey(userId))
+                    if (!this.totalUsers.containsKey(userId)){
                         this.totalUsers.put(userId, totalUsers.size());
+                    }
                     this.totalReviews++;
+
+                    lineToWrite = this.totalUsers.get(userId) + "," + selectedProduct + ",";
+                } else if (lineToRead.startsWith(SCORE_PATTERN)) {
+                    double score = Double.parseDouble(lineToRead.substring(SCORE_BEGIN_INDEX));
+                    lineToWrite += score + "\n";
+                    writer.write(lineToWrite);
+                    writer.flush();
                 }
             }
 
             reader.close();
+            writer.close();
 
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        return new File(DATABASE);
+    }
+
+    public void buildRecommender(File database) {
+        try {
+            DataModel model = new FileDataModel(database);
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+            this.recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
+
 
     public int getTotalReviews() {
         return totalReviews;
@@ -62,7 +117,23 @@ public class MovieRecommender {
         return totalUsers.size();
     }
 
-    public List<String> getRecommendationsForUser(String id) {
+    public List<String> getRecommendationsForUser(String id)  {
+        try {
+            return getRecommendations(this.recommender.recommend(this.totalUsers.get(id), 3));
+        } catch (TasteException ex) {
+            ex.printStackTrace();
+        }
+
         return null;
+    }
+
+    private List<String> getRecommendations(List<RecommendedItem> recommendations) {
+        ArrayList<String> products = new ArrayList<String>();
+
+        for (RecommendedItem r : recommendations) {
+            products.add(this.totalProducts.inverse().get((int) r.getItemID()));
+        }
+
+        return products;
     }
 }
